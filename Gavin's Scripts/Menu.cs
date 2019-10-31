4caf06593291ca3d614c953ca2f1fbbb2cc61ea3
Gavin Yentis <maldov@aol.com>
2019-10-30 17:03:42 -0700

    added scripts

---
+++ Gavin's Scripts/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public int buttonWidth;
+    public int buttonHeight;
+    private int origin_x;
+    private int origin_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonWidth = 200;
+        buttonHeight = 50;
+        origin_x = Screen.width / 2 - buttonWidth / 2;
+        origin_y = Screen.height / 2 - buttonHeight * 2;
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(origin_x, origin_y, buttonWidth, buttonHeight), "Game 1"))
+        {
+            Application.LoadLevel(1);
+        }
+        if (GUI.Button(new Rect(origin_x, origin_y + buttonHeight + 10, buttonWidth, buttonHeight), "Game 2"))
+        {
+            Application.LoadLevel(2);
+        }
+        if (GUI.Button(new Rect(origin_x, origin_y + buttonHeight * 2 + 20, buttonWidth, buttonHeight), "Exit"))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+    			Application.Quit();
+#endif
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
