3e9e7920e1cf165d01780a41ae231213d6dab12e
japancakes <japancakes@live.com>
2019-11-02 18:40:45 -0700

    Created basic first level outline enviornment for the first level

---
+++ Assets/Scripts/Gavin's Scripts/EnemyController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    public Transform blade;
+
+    public float speed;
+    public int closeDist;
+    public int bladeSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        transform.LookAt(target, Vector3.up);
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (dist < closeDist)
+        {
+            blade.Rotate(new Vector3(0,bladeSpeed,0), Space.World);
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
+
+
+
+
+
