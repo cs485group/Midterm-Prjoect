799d3dd0684184b05a4d93c2d5502009ac6a48b9
wolffire2077 <56852419+wolffire2077@users.noreply.github.com>
2019-11-04 10:48:05 -0800

    Add files via upload

---
+++ Assets/Scripts/BlobScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobScript : MonoBehaviour
+{
+    private bool isDead = false;
+    private bool grounded = true;
+    private Rigidbody rbody;
+    public LayerMask ground;
+    public Transform Player;
+    public Transform Blob;
+    public GameObject PlayerObject;
+    private Animator anim;
+
+    int MoveSpeed = 2;
+    int MaxDist = 5;
+    int MinDist = 0;
+
+    public int buttonWidth;
+    public int buttonHeight;
+    private int origin_x;
+    private int origin_y;
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        buttonWidth = 200;
+        buttonHeight = 50;
+        origin_x = Screen.width / 2 - buttonWidth / 2;
+        origin_y = Screen.height / 2 - buttonHeight * 2;
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+        
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist & isGrounded())
+        {
+  
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            anim.Play("Bounce",MoveSpeed);
+         
+ 
+
+            
+ 
+
+        }
+
+        bool isGrounded()
+        {
+            if (Physics.CheckSphere(Blob.position, .8f, ground, QueryTriggerInteraction.Ignore))
+            {
+   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Destroy(PlayerObject);
+            isDead = true;
+
+        }
+    }
+    public void OnGUI()
+    {
+        if (isDead == true)
+        {
+            if (GUI.Button(new Rect(origin_x, origin_y + buttonHeight * 2 + 20, buttonWidth, buttonHeight), "Restart"))
+            {
+                Application.LoadLevel("ThirdPerson");
+            }
+            if (GUI.Button(new Rect(origin_x, origin_y + buttonHeight * 3 + 30, buttonWidth, buttonHeight + 10), "Exit"))
+            {
+                UnityEditor.EditorApplication.isPlaying = false;
+                Application.Quit();
+
+            }
+        }
+        else
+        {
+
+        }
+
+    }
+}
