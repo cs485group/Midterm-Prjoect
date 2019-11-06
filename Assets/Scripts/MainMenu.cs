a2eace6af8af4d472b24143260b9b39969ac2dbe
cs485group <56851451+cs485group@users.noreply.github.com>
2019-11-06 13:17:35 -0800

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -19,26 +19,4 @@ public class MainMenu : MonoBehaviour
     }
 
 
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MainMenu : MonoBehaviour
-{
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-    public void QuitGame()
-    {
-        Debug.Log("QUIT!");
-        Application.Quit();
-     
-
-    }
-
-
->>>>>>> 7aa8d6c01a97ae8836ff0725870ad58804c12366
 }
