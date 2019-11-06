b85d292f6505fac012b071df14243484923ea9b7
japancakes <japancakes@live.com>
2019-11-06 10:51:31 -0800

    Added Damien's files

---
+++ Assets/Scripts/Health&damagingscript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ehealthbar : MonoBehaviour
+{
+
+    public AudioSource audioClip;
+    public float health;
+    public float maxHealth;
+    public GameObject healthBarUI;
+    public Slider slider;
+    public float SwordDamage;
+    public float HammerDamage;
+    public GameObject PlayerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = 4;
+        health = maxHealth;
+        SwordDamage = 0;
+        HammerDamage = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = health;
+        if (health < maxHealth)
+        {
+
+            healthBarUI.SetActive(true);
+        }
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+
+
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Sword")
+        {
+            SwordDamage = 1;
+            health = health = SwordDamage;
+        }
+
+
+
+
+        if (other.gameObject.tag == "Hammer")
+        {
+            HammerDamage = 2;
+            health = health - HammerDamage;
+
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Sword"))
+
+        {
+            SwordDamage = 1;
+            health = health -SwordDamage;
+            audioClip.Play();
+        }
+        if (collision.collider.CompareTag("Hammer"))
+
+        {
+            HammerDamage = 2;
+            health = health - HammerDamage;
+        }
+    }
+}
