57638d9e15c36ec942481ecd1aede48f0fa66c48
Luxuno <jedi.lol@hotmail.com>
2019-12-08 16:21:37 -0800

    Updated PlayerModel w/ Anims and HUD

---
+++ Assets/Models and Animations/UpdatedPlayer/Anim.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    public Animator fullBody;
+    
+    public GameObject sword;
+    public GameObject hammer;
+    public GameObject spear;
+
+    public float timer;
+    private int state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fullBody.SetBool("RUN", true);
+        state = 0;
+        timer = 0;
+        sword.SetActive(false);
+        hammer.SetActive(false);
+        spear.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 3f)
+        {
+            state = (state + 1)%4;
+            timer = 0;
+        }
+
+        if(state == 1)
+        {
+            sword.SetActive(true);
+            fullBody.SetBool("Sword", true);
+        }
+        else if(state == 2)
+        {
+            fullBody.SetBool("Sword", false);
+            sword.SetActive(false);
+            hammer.SetActive(true);
+            fullBody.SetBool("2h",true);
+        }
+        else if(state == 3)
+        {
+            hammer.SetActive(false);
+            spear.SetActive(true);
+        }
+        else
+        {
+            spear.SetActive(false);
+            fullBody.SetBool("2h",false);
+        }
+
+    }
+}
---
+++ Assets/Models and Animations/UpdatedPlayer/TestCactus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCactus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damageC;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+           collision.gameObject.GetComponent<PlayerController>().HurtPlayer(damageC);
+        }
+    }
+}
